d67ea0e5c2bd6dd53bbd88863132ebc9c5e6c710
Riley Bowling <rbowling@bsd220.org>
2019-12-12 10:51:38 -0600

    Initial Commit

---
+++ semester 1 app 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  semester 1 app 2.0
+//
+//  Created by Riley Bowling on 12/12/19.
+//  Copyright © 2019 Riley Bowling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
